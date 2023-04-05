eeb4a87a4fb6cd80c16fb6f206113a1792d7b45f
Jaime Villegas <jaimevillegas296@gmail.com>
2023-04-05 16:46:17 -0500

    Work on Bookstore API

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,16 +1,24 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/booksSlice';
+import { getBookList, removeBook } from '../redux/getApiData';
 
-export default function Book({ title, author, id }) {
+export default function Book({ title, author, itemId }) {
   const dispatch = useDispatch();
+  const URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/';
+
+  const dispatchRemove = () => {
+    dispatch(removeBook({ URL, itemId }))
+      .then(() => {
+        dispatch(getBookList(URL));
+      });
+  };
 
   return (
     <div className="book-container">
       <p>{title}</p>
       <p>{author}</p>
-      <button type="button" onClick={() => dispatch(removeBook(id))}>Remove</button>
+      <button type="button" onClick={() => dispatchRemove()}>Remove</button>
     </div>
   );
 }
@@ -18,5 +26,5 @@ export default function Book({ title, author, id }) {
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
+  itemId: PropTypes.string.isRequired,
 };
